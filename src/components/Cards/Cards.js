519eb9b492e89ff57e23876eff0e7bb12db45c30
srikant <srikantvs26@gmail.com>
2022-08-31 14:28:38 +0530

    cards completed with status change

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -7,24 +7,44 @@ const Cards = ({ results }) => {
     let display;
     if (results) {
         display = results.map(eachRes => {
-            let { id ,name,image,location,status} = eachRes;
+            let { id, name, image, location, status } = eachRes;
             return (
-                <div key={id} className="col-4 position-relative mb-3">
+                <div key={id} className="col-4 position-relative mb-4">
                     <div className={styles.cards}>
                         <img src={image} alt="" className={`img-fluid ${styles.img}`} />
-                        <div className="content p-1">  
+                        <div className="content p-1">
                             <div className="fs-4 fw-bold mb-4 text-truncate" title={name} >{name}</div>
                             <div className="">
                                 <div className="fs-6">Last Location</div>
                                 <div className="fs-5 text-truncate" title={location.name}>{location.name}</div>
-                                
                             </div>
                         </div>
                     </div>
-                    <div className={`${styles.badge} badge text-bg-danger position-absolute text-uppercase`}>{status}</div>
+                    
+                    {
+                        
+                        (() => {
+                            if (status === "Dead") {
+                                return (
+                                    <div className={`${styles.badge} badge text-bg-danger position-absolute text-uppercase`}>{status}</div>
+                                );
+                            }
+                            else if (status === "Alive") {
+                                return (
+                                    <div className={`${styles.badge} badge text-bg-success position-absolute text-uppercase`}>{status}</div>
+                                );
+                            }
+                            else {
+                                return (
+                                    <div className={`${styles.badge} badge text-bg-secondary position-absolute text-uppercase`}>{status}</div>
+                                );
+                            }
+                        })()
+                   }
+                    
                 </div>
             );
-         });
+        });
     }
     else {
         display = "No Characters Found :/";
